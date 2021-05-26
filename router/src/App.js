c1a0aa0f8cbd2c0c6e858dfd48b05865a3ce1a1a
Joke Heyndels <jo_hannah@outlook.com>
2021-05-26 10:41:51 +0000

    Add a Navbar

--- router/src/App.js
+++ router/src/App.js
@@ -1,4 +1,4 @@
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Link} from 'react-router-dom'
 import Test from "./Test"
 import Home from "./Home"
 import './App.css';
@@ -6,6 +6,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/test">Test</Link>
+      </nav>
       <Switch> 
         <Route exact path="/" component={Home}/>
         <Route exact path="/test" component={Test} />
