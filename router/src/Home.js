edfc74c6b46870c1138cf37797634624d70a4380
Joke Heyndels <jo_hannah@outlook.com>
2021-05-26 10:25:25 +0000

    Adding routing. Problem: Home component keeps showing

---
+++ router/src/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Test extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home Page goes here!</h1>
+            </div>
+        )
+    }
+}
+
+export default Test
