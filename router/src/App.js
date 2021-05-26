896edb9e45921d523d922ba9bac979e2518aa978
Joke Heyndels <jo_hannah@outlook.com>
2021-05-26 10:32:53 +0000

    implement Routing

--- router/src/App.js
+++ router/src/App.js
@@ -1,4 +1,4 @@
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Test from "./Test"
 import Home from "./Home"
 import './App.css';
@@ -6,10 +6,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={Home}/>
-      <Route path="/test" component={Test} />
+      <Switch> 
+        <Route path="/test" component={Test} />
+        <Route path="/" component={Home}/>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+/**
+ * Caution: for Switch, order matters. First matching route will show. 
+ * Alternative: use exact path - Can be used together with Switch
+ **/
