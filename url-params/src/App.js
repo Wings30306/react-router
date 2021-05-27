f6a8334e16b3c3ad7a5a2e77ba2bab6403d68d7a
Joke Heyndels <jo_hannah@outlook.com>
2021-05-27 13:29:14 +0000

    Access params from the url in a component

--- url-params/src/App.js
+++ url-params/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, {Component} from "react"
+import { Route } from 'react-router-dom';
 import './App.css';
+import Food from './Food';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Route 
+          exact 
+          path="/food/:name" 
+          render={(routeProps) => <Food name={routeProps.match.params.name} />}
+        />
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
